ad1a6a1c4ba92412c38b47472c9a75ebb8328a04
unknown <blur-imran@hotmail.com>
2021-03-24 17:29:24 +0600

    1

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function imgSlider(i){
+    document.querySelector('.starbucks').src = i;
+}
+function colorChange(color){
+    const circle =  document.querySelector('.circle');
+    circle.style.background = color;
+}
